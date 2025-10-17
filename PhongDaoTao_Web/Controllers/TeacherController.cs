340c8d00c52218914fcd86cc47b1d749906a63fb
phamtrungtin8049 <bourbon8049@gmail.com>
2025-10-17 09:03:31 +0700

    update

--- PhongDaoTao_Web/Controllers/TeacherController.cs
+++ PhongDaoTao_Web/Controllers/TeacherController.cs
@@ -6,49 +6,9 @@ namespace PhongDaoTao_Web.Controllers
 {
     public class TeacherController : Controller
     {
-        private readonly ApplicationDbContext _context;
-
-        public TeacherController(ApplicationDbContext context)
+        public IActionResult Index()
         {
-            _context = context;
-        }
-
-        // 🔹 Xem thời khóa biểu
-        public async Task<IActionResult> ThoiKhoaBieu(string maGiaoVien)
-        {
-            var lopDay = await _context.Classrooms
-                .Include(l => l.Schedules)
-                .Where(l => l.TeacherID == maGiaoVien)
-                .ToListAsync();
-
-            return View(lopDay);
-        }
-
-
-        // 🔹 Nhập điểm
-        public async Task<IActionResult> NhapDiem(int maLop)
-        {
-            var danhSach = await _context.Score
-                .Include(d => d.HocVien)
-                .Where(d => d.MaLop == maLop)
-                .ToListAsync();
-
-            ViewBag.Lop = await _context.Classrooms.FindAsync(maLop);
-            return View(danhSach);
-        }
-
-        [HttpPost]
-        public async Task<IActionResult> CapNhatDiem(int maDiem, double diemGiuaKy, double diemCuoiKy, string nhanXet)
-        {
-            var diem = await _context.Score.FindAsync(maDiem);
-            if (diem == null) return NotFound();
-
-            diem.DiemGiuaKy = diemGiuaKy;
-            diem.DiemCuoiKy = diemCuoiKy;
-            diem.NhanXet = nhanXet;
-
-            await _context.SaveChangesAsync();
-            return RedirectToAction(nameof(NhapDiem), new { maLop = diem.MaLop });
+            return View();
         }
     }
 }
